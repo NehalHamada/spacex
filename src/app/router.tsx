b378dad2daf21a03f404a5952b75fe05c0603894
NehalHamada <nehalhamada3@gmail.com>
2026-05-08 15:57:40 +0300

    update

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -10,7 +10,7 @@ function AppRoutes() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/details/:id" element={<LaunchDetailsPage />} />
-        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
