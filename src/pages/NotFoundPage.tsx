42e928777679ac550ae8c8ca6dbe14b58adf2af9
NehalHamada <nehalhamada3@gmail.com>
2026-05-08 16:12:30 +0300

    fix

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,7 +2,7 @@ import not from "/error.png";
 function NotFoundPage() {
   return (
     <div>
-      <img src={not} alt="" className="mx-auto block w-full" />
+      <img src={not} alt="" className="mx-auto block w-80" />
     </div>
   );
 }
